a00942940749b065d50534faae2e18a83bd3f1a2
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2022-03-16 00:35:52 +0300

    feat: update example to match new API

--- example/example.dart
+++ example/example.dart
@@ -1,21 +1,42 @@
+// ignore_for_file: avoid_print, unused_local_variable
+
 import 'dart:io';
-// Import and shot prefixed typedefs
-import 'package:nhentai/nhentai.dart' show NHentai, NHentaiHosts, NHentaiBook, NHentaiSearch;
 // Import with library prefix
 import 'package:nhentai/nhentai.dart' as nh;
+// Import prefixed version
+import 'package:nhentai/nhentai_prefixed.dart';
 
 Future<void> main() async {
-  final apiProxy = nh.API(
-    client: HttpClient() // Using HTTP proxy
-      ..addProxyCredentials('example.com', 1337, 'Basic', HttpClientBasicCredentials('0xdeadbeef', '0x0badf00d'))
-      ..findProxy = (uri) => 'PROXY example.com:1337',
+  // API client
+  final pureApi = nh.API();
+
+  // Client with HTTP proxy
+  final proxyApi = NHentai.proxy(
+    'http://0xdeadbeef:0x0badf00d@example.com:1337/',
     hosts: const NHentaiHosts(
       images    : ['i2.nhentai.net'],
       thumbnails: ['t7.nhentai.net'],
     ),
   );
-  // Or just use default settings:
-  final api = NHentai();
+
+  // Client with custom HTTP Client
+  final customClientApi = nh.API(
+    // HTTP proxy example
+    client: HttpClient()
+      ..addProxyCredentials(
+        'example.com',
+        1337,
+        'Basic',
+        HttpClientBasicCredentials('0xdeadbeef', '0x0badf00d'),
+      )
+      ..findProxy = (uri) => 'PROXY example.com:1337',
+    hosts: const NHentaiHosts(
+      images    : ['i5.nhentai.net'],
+      thumbnails: ['t2.nhentai.net'],
+    ),
+  );
+
+  final api = pureApi;
 
   final NHentaiBook? book = await api.getBook(177013);
   if (book == null)
@@ -35,19 +56,26 @@ Future<void> main() async {
 
   // Search for books with the same language
   // Print names of first 2 books of first 2 pages of search
-  await for (final search in api.searchTagged(book.tags.languages.first)) {
+  await for (final search in api.searchTagged(book.tags.languages.first, count: 2)) {
     print(search);
     print(search.books.take(2).join(', '));
-    if (search.page >= 2)
-      break;
+
+    // This is bad, because the stream are already requesting new page at this
+    // time. Use `count` option instead of breaking by condition like this.
+    // if (search.page >= 2)
+    //   break;
   }
 
   // Make single query and print name of first book
-  final NHentaiSearch search1 = await api.search('test').first;
+
+  // Request via generator, here you must specify count to 1,
+  // otherwise you'll make and will wait for 2 requests instead of single one.
+  final nh.Search search1 = await api.search('test', count: 1).first;
   print(search1.books.first);
-  // or
-  final nh.Search? search2 = await api.doSearch('test');
+
+  // Pure single search request
+  final NHentaiSearch? search2 = await api.searchSinglePage('test');
   print(search2!.books.first);
 
-  exit(0);
+  return;
 }
