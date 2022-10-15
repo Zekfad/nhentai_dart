440ea2b6b830ecae3da120741aaf8e98a2acfb60
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2022-10-15 19:04:08 +0300

    chore: add comments example

---
+++ example/comments.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: avoid_print, unused_local_variable, omit_local_variable_types
+
+// Working with tags
+import 'package:nhentai/nhentai.dart' as nh;
+
+
+final api = nh.API();
+
+Future<void> main() async {
+  final List<nh.Comment>? comments = await api.getComments(177013);
+  if (comments == null)
+    throw Exception('Something went wrong.');
+
+  final nh.Comment comment = comments.first;
+  // Print first comment summary
+  print(
+    '${comment.author.username} (${comment.date}): '
+    '${comment.body}'
+  );
+}
