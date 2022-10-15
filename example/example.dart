97ba49b63654f87023c4ae11df98b1534a28e606
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2022-10-15 18:10:19 +0300

    chore: update exmaples

--- example/example.dart
+++ example/example.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, unused_local_variable
+// ignore_for_file: avoid_print, unused_local_variable, omit_local_variable_types
 
 import 'dart:io';
 // Import with library prefix
@@ -6,22 +6,25 @@ import 'package:nhentai/nhentai.dart' as nh;
 // Import prefixed version
 import 'package:nhentai/nhentai_prefixed.dart';
 
+
 Future<void> main() async {
-  // API client
+  // API client with default parameters.
   final pureApi = nh.API();
 
-  // Client with HTTP proxy
+  // API client with HTTP proxy.
   final proxyApi = NHentai.proxy(
+    // Proxy URI.
     'http://0xdeadbeef:0x0badf00d@example.com:1337/',
-    hosts: const NHentaiHosts(
-      images    : ['i2.nhentai.net'],
-      thumbnails: ['t7.nhentai.net'],
+    // Custom hosts config with changed image and thumbnail hosts.
+    hosts: NHentaiHosts.only(
+      image    : const NHentaiImageHost('i2.nhentai.net'),
+      thumbnail: const NHentaiThumbnailHost('t7.nhentai.net'),
     ),
   );
 
-  // Client with custom HTTP Client
+  // Client with custom HTTP Client.
   final customClientApi = nh.API(
-    // HTTP proxy example
+    // HTTP client with proxy.
     client: HttpClient()
       ..addProxyCredentials(
         'example.com',
@@ -30,9 +33,14 @@ Future<void> main() async {
         HttpClientBasicCredentials('0xdeadbeef', '0x0badf00d'),
       )
       ..findProxy = (uri) => 'PROXY example.com:1337',
-    hosts: const NHentaiHosts(
-      images    : ['i5.nhentai.net'],
-      thumbnails: ['t2.nhentai.net'],
+    // Multiple hosts config with 2 image hosts.
+    hosts: NHentaiHosts.multiple(
+      image: [
+        // Create image host via dedicated class constructor.
+        const NHentaiImageHost('i7.nhentai.net'),
+        // Create image host config via generic host factory.
+        NHentaiHost(NHentaiHostType.image, 'i5.nhentai.net') as NHentaiImageHost,
+      ],
     ),
   );
 
@@ -42,7 +50,7 @@ Future<void> main() async {
   if (book == null)
     throw Exception('Something went wrong.');
   
-  // Short book summary
+  // Print short book summary.
   print(
     'Book: $book\n'
     'Artists: ${book.tags.artists.join(', ')}\n'
@@ -54,26 +62,27 @@ Future<void> main() async {
 
   print('\n');
 
-  // Search for books with the same language
-  // Print names of first 2 books of first 2 pages of search
+  // Search for books with the same language.
+  // Print names of first 2 books of first 2 pages of search.
   await for (final search in api.searchTagged(book.tags.languages.first, count: 2)) {
     print(search);
     print(search.books.take(2).join(', '));
 
-    // This is bad, because the stream are already requesting new page at this
-    // time. Use `count` option instead of breaking by condition like this.
+    // This is a bad approach because the stream is already in process of
+    // requesting new page at this point.
+    // Use `count` option instead of breaking by condition like this.
     // if (search.page >= 2)
     //   break;
   }
 
-  // Make single query and print name of first book
+  // Make a single query and print name of the first book.
 
-  // Request via generator, here you must specify count to 1,
-  // otherwise you'll make and will wait for 2 requests instead of single one.
+  // Request via generator, here you must specify `count` to 1, otherwise stream
+  // will make 2 request and you'll wait for 2 requests instead of single one.
   final nh.Search search1 = await api.search('test', count: 1).first;
   print(search1.books.first);
 
-  // Pure single search request
+  // Pure single search request.
   final NHentaiSearch? search2 = await api.searchSinglePage('test');
   print(search2!.books.first);
 
--- example/tags.dart
+++ example/tags.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: avoid_print, unused_local_variable
+// ignore_for_file: avoid_print, unused_local_variable, omit_local_variable_types
 
 // Working with tags
 import 'package:nhentai/nhentai.dart' as nh;
+
 
 final api = nh.API();
 
@@ -11,21 +12,27 @@ Future<void> main() async {
     throw Exception('Something went wrong.');
   final tags = book.tags;
 
-  // using predefined filters
+  // Use predefined filters
   print(tags.artists);
 
-  // getting certain types
+  // Get certain types of tags
   print(tags.get([nh.TagType.artist, nh.TagType.language]));
 
-  // excluding certain types
+  // Exclude certain tag types
   print(tags.exclude([nh.TagType.group, nh.TagType.language]));
 
-  // creating modifiable list of tags
+  // Obtain modifiable list of tags
   print(tags.toList());
 
-  // combine tags using modifiable list of tags
+  // Combine tags using modifiable list of tags
   print(tags.artists.toList()..addAll(tags.categories));
-  // equivalent to (almost, because this one returns unmodifiable list)
+  // or
+  print([
+    ...tags.artists,
+    ...tags.categories,
+  ]);
+
+  // Almost the same as this (this one returns unmodifiable list)
   print(tags.get([nh.TagType.artist, nh.TagType.category]));
 
   return;
