5ba849d3e4af06a56f3aecd5e6e0e185a023f7d4
Yaroslav Vorobev <zekfad@znnme.eu.org>
2023-07-22 02:24:25 +0300

    chore: remove old code

--- lib/src/models/images/encode_image_hook.dart
+++ lib/src/models/images/encode_image_hook.dart
@@ -17,19 +17,6 @@ class EncodeImageHook extends MappingHook {
         'w': width,
       't': const ImageTypeMapper().encode(value.type),
     },
-    // Image() => value,
     _ => throw MapperException.unexpectedType(value.runtimeType, 'Image'),
   };
-  // @override
-  // dynamic afterEncode(dynamic value) => switch(value) {
-  //   Image() => {
-  //     if (value.height case final height?)
-  //       'h': height,
-  //     if (value.width case final width?)
-  //       'w': width,
-  //     't': value.type,
-  //     'dsadsadsa': 'dsdsad',
-  //   },
-  //   _ => throw MapperException.unexpectedType(value.runtimeType, 'Image'),
-  // };
 }
