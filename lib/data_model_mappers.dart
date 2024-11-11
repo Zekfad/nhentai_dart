fafe4212e1182b232b3821449fbcec72f189304a
Anatoly Shalyov <lacticwhale@gmail.com>
2024-11-11 12:03:21 +0300

    feat: no more exposed internals

--- lib/data_model_mappers.dart
+++
@@ -1,6 +0,0 @@
-/// Model mapper library.
-/// It contains model mappers for API objects, such as [BookMapper]s, [TagMapper]s, [ImageMapper]s,
-/// and so on.
-library nhentai_data_model_mapper;
-
-export 'src/model_mappers.dart';
